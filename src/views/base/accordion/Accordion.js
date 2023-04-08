527cf8ca98d886354f051253c655d7f05d0a22e7
tuyen10a6 <phamtuyenok2002@gmail.com>
2023-04-09 00:37:23 +0700

    create update product

--- src/views/base/accordion/Accordion.js
+++ src/views/base/accordion/Accordion.js
@@ -118,16 +118,24 @@ const Accordion = () => {
                           <Modal.Body>
                             <Form>
                               <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-                                <Form.Label>Email address</Form.Label>
-                                <Form.Control
-                                  type="email"
-                                  placeholder="name@example.com"
-                                  autoFocus
-                                />
+                                <Form.Label>Tên sản phẩm</Form.Label>
+                                <Form.Control type="text" placeholder="" autoFocus />
                               </Form.Group>
                               <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                                <Form.Label>Example textarea</Form.Label>
-                                <Form.Control as="textarea" rows={3} />
+                                <Form.Label>Miêu tả</Form.Label>
+                                <Form.Control as="textarea" rows={1} />
+                              </Form.Group>
+                              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Nhãn hiệu</Form.Label>
+                                <Form.Control as="textarea" rows={1} />
+                              </Form.Group>
+                              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Loại sản phẩm</Form.Label>
+                                <Form.Control as="textarea" rows={1} />
+                              </Form.Group>
+                              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                                <Form.Label>Hình ảnh</Form.Label>
+                                <Form.Control type="file" />
                               </Form.Group>
                             </Form>
                           </Modal.Body>
