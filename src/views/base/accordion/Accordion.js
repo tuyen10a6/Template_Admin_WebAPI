395df1f38776f7a44cef795509151099c9f36b23
tuyen10a6 <phamtuyenok2002@gmail.com>
2023-04-11 19:15:14 +0700

    upload file Image successfully

--- src/views/base/accordion/Accordion.js
+++ src/views/base/accordion/Accordion.js
@@ -9,6 +9,8 @@ import Table from 'react-bootstrap/Table'
 import Button from 'react-bootstrap/Button'
 import Form from 'react-bootstrap/Form'
 import Modal from 'react-bootstrap/Modal'
+import { toast, ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 import {
   CCard,
   CCardBody,
@@ -33,41 +35,33 @@ const Accordion = () => {
   //
   const [show, setShow] = useState(false)
   const handleClose = () => setShow(false)
+  const [showSecond, setShowSecond] = useState(false)
+
+  const handleCloseSecond = () => setShowSecond(false)
+  const handleShowSecond = () => setShowSecond(true)
 
   // const handleClose = () => setShow(false)
-  const handleSave = () => {
-    fetch(`https://localhost:7014/api/SanPham/EditSanPham`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        productID: selectedProduct.productID,
-        productName: selectedProduct.productName,
-        description: selectedProduct.description,
-        imageURL: selectedProduct.imageURL,
-        categoryID: selectedProduct.categoryID,
-        brandID: 1,
-        categoryName: 'string',
-        price: 0,
-      }),
-    })
-      .then((response) => {
-        if (response.ok) {
-          console.log('Product updated successfully')
-          handleClose()
-        } else {
-          console.log('Error updating product')
-        }
-      })
-      .catch((error) => console.error(error))
-  }
 
   // Tạo state lưu trữ toàn bộ loại sản phẩm
 
   const [category, setCategory] = useState([])
   const [categoryIdMap, setCategoryIdMap] = useState({})
+  const [brand, setbrand] = useState([])
+  const [brandIdMap, setbrandIdMap] = useState({})
 
+  useEffect(() => {
+    fetch('https://localhost:7014/api/Brand/GetAllBrands')
+      .then((res) => res.json())
+      .then((data) => {
+        setbrand(data)
+        const idMap = {}
+        data.forEach((brand) => {
+          idMap[brand.brandName] = brand.brandID
+        })
+        setbrandIdMap(idMap)
+        console.log('Call Api GetAllBrand Successfully')
+      })
+  }, [])
   useEffect(() => {
     fetch('https://localhost:7014/apiUser/Categories/GetAllCategories')
       .then((res) => res.json())
@@ -100,17 +94,106 @@ const Accordion = () => {
     fetchDataProduct()
   }, [pageNumber, pageSize])
 
-  /// Image Product Change
-  // const [imageSrc, setImageSrc] = useState(null)
-  // const handleImageChange = (e) => {
-  //   const file = e.target.files[0]
-  //   const reader = new FileReader()
-  //   reader.onload = (event) => {
-  //     setImageSrc(event.target.result)
-  //   }
-  //   reader.readAsDataURL(file)
-  // }
+  // Hàm thêm
 
+  const handleAdd = () => {
+    const ProductName = document.getElementById('Text_ProductName').value.length
+    const Description = document.getElementById('Text_description').value.length
+
+    if (ProductName === 0 || Description === 0) {
+      alert('Vui lòng nhập đầy đủ thông tin sản phẩm')
+      console.log('ProductName Null')
+      return
+    }
+
+    const fileInput = document.querySelector('#File_image')
+    const file = fileInput.files[0]
+
+    if (!file) {
+      alert('Vui lòng chọn hình ảnh sản phẩm.')
+      return
+    }
+
+    const formData = new FormData()
+    formData.append('file', file)
+
+    fetch('https://localhost:7014/api/TestUpLoadFile/upload/product', {
+      method: 'POST',
+      body: formData,
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const imageURL = `https://localhost:7014${data.filePath}`
+
+        fetch(`https://localhost:7014/api/SanPham/AddSanPham`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify({
+            productID: 0,
+            productName: selectedProduct.productName,
+            description: selectedProduct.description,
+            imageURL: imageURL,
+            categoryID: selectedProduct.categoryID,
+            brandID: selectedProduct.brandID,
+            categoryName: 'string',
+            price: 0,
+            brandName: 'string',
+          }),
+        })
+          .then((response) => {
+            if (response.ok) {
+              console.log('Product ADD successfully')
+              handleCloseSecond()
+              alert('Thêm sản phẩm thành công !')
+            } else {
+              console.log('Error Add product')
+            }
+          })
+          .catch((error) => console.error(error))
+      })
+      .catch((error) => console.error(error))
+  }
+
+  // Hàm sửa
+  const handleSave = () => {
+    fetch(`https://localhost:7014/api/SanPham/EditSanPham`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        productID: selectedProduct.productID,
+        productName: selectedProduct.productName,
+        description: selectedProduct.description,
+        imageURL: selectedProduct.imageURL,
+        categoryID: selectedProduct.categoryID,
+        brandID: selectedProduct.brandID,
+        categoryName: 'string',
+        price: 0,
+        brandName: 'string',
+      }),
+    })
+      .then((response) => {
+        if (response.ok) {
+          console.log('Product updated successfully')
+          handleClose()
+          notify()
+          setDataProduct(
+            DataProduct.map((product) => {
+              if (product.productID === selectedProduct.productID) {
+                return selectedProduct
+              }
+              return product
+            }),
+          )
+        } else {
+          console.log('Error updating product')
+        }
+      })
+      .catch((error) => console.error(error))
+  }
   function handlePageChange(page) {
     if (page !== totalPages) {
       setPageNumber(page)
@@ -137,6 +220,16 @@ const Accordion = () => {
         })
     }
   }
+  function SaveUpdateProduct() {
+    handleSave()
+    notify()
+  }
+  // Notify
+  const notify = () => {
+    toast.success('Sửa thành công !', {
+      position: toast.POSITION.TOP_RIGHT,
+    })
+  }
 
   return (
     <CRow>
@@ -144,6 +237,120 @@ const Accordion = () => {
         <CCard className="mb-4">
           <CCardHeader>
             <strong>Sản phẩm</strong>
+
+            <button
+              onClick={handleShowSecond}
+              style={{
+                background: '#ccc',
+                border: 'none',
+                padding: '5px 10px',
+                borderRadius: '6px',
+                marginLeft: '20px',
+              }}
+            >
+              {' '}
+              Thêm mới
+            </button>
+            <Modal show={showSecond} onHide={handleCloseSecond} backdrop="static" keyboard={false}>
+              <Modal.Header closeButton>
+                <Modal.Title>Thêm sản phẩm</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                  <Form.Label>Tên sản phẩm</Form.Label>
+                  <Form.Control
+                    id="Text_ProductName"
+                    type="text"
+                    placeholder=""
+                    autoFocus
+                    value={selectedProduct ? selectedProduct.productName : ''}
+                    onChange={(e) =>
+                      setSelectedProduct({
+                        ...selectedProduct,
+                        productName: e.target.value,
+                      })
+                    }
+                  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                  <Form.Label>Miêu tả</Form.Label>
+                  <Form.Control
+                    id="Text_description"
+                    type="text"
+                    placeholder=""
+                    autoFocus
+                    value={selectedProduct ? selectedProduct.description : ''}
+                    onChange={(e) =>
+                      setSelectedProduct({
+                        ...selectedProduct,
+                        description: e.target.value,
+                      })
+                    }
+                  />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="exampleForm.ControlSelect1">
+                  <Form.Label>Loại sản phẩm</Form.Label>
+                  <Form.Control
+                    as="select"
+                    value={selectedProduct ? selectedProduct.categoryName : ''}
+                    onChange={(e) =>
+                      setSelectedProduct({
+                        ...selectedProduct,
+                        categoryID: categoryIdMap[e.target.value],
+                        categoryName: e.target.value,
+                      })
+                    }
+                  >
+                    <option value="">Chọn loại sản phẩm</option>
+                    {category.map((category) => (
+                      <option key={category.categoryID} value={category.categoryName}>
+                        {category.categoryName}
+                      </option>
+                    ))}
+                  </Form.Control>
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlSelect1">
+                  <Form.Label>Thương Hiệu</Form.Label>
+                  <Form.Control
+                    as="select"
+                    value={selectedProduct ? selectedProduct.brandName : ''}
+                    onChange={(e) =>
+                      setSelectedProduct({
+                        ...selectedProduct,
+                        brandID: brandIdMap[e.target.value],
+                        brandName: e.target.value,
+                      })
+                    }
+                  >
+                    <option value="">Chọn thương hiệu</option>
+                    {brand.map((brands) => (
+                      <option key={brands.brandID} value={brands.brandName}>
+                        {brands.brandName}
+                      </option>
+                    ))}
+                  </Form.Control>
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                  <Form.Label>Hình ảnh</Form.Label>
+                  <Form.Control
+                    // onChange={handleImageChange}
+                    id="File_image"
+                    required="required"
+                    type="file"
+                  />
+                </Form.Group>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button variant="secondary" onClick={handleCloseSecond}>
+                  Đóng
+                </Button>
+                <Button onClick={handleAdd} variant="primary">
+                  Thêm
+                </Button>
+              </Modal.Footer>
+            </Modal>
           </CCardHeader>
           <CCardBody>
             <Table striped bordered hover>
@@ -231,15 +438,7 @@ const Accordion = () => {
                                   }
                                 />
                               </Form.Group>
-                              {/* <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-                                  <Form.Label>Loại sản phẩm</Form.Label>
-                                  <Form.Control
-                                    value={item.categoryName}
-                                    id="Text_Brand"
-                                    as="select"
-                                    rows={1}
-                                  />
-                                </Form.Group> */}
+
                               <Form.Group className="mb-3" controlId="exampleForm.ControlSelect1">
                                 <Form.Label>Loại sản phẩm</Form.Label>
                                 <Form.Control
@@ -261,12 +460,34 @@ const Accordion = () => {
                                   ))}
                                 </Form.Control>
                               </Form.Group>
+                              <Form.Group className="mb-3" controlId="exampleForm.ControlSelect1">
+                                <Form.Label>Thương Hiệu</Form.Label>
+                                <Form.Control
+                                  as="select"
+                                  value={selectedProduct ? selectedProduct.brandName : ''}
+                                  onChange={(e) =>
+                                    setSelectedProduct({
+                                      ...selectedProduct,
+                                      brandID: brandIdMap[e.target.value],
+                                      brandName: e.target.value,
+                                    })
+                                  }
+                                >
+                                  <option value="">Chọn thương hiệu</option>
+                                  {brand.map((brands) => (
+                                    <option key={brands.brandID} value={brands.brandName}>
+                                      {brands.brandName}
+                                    </option>
+                                  ))}
+                                </Form.Control>
+                              </Form.Group>
 
                               <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                                 <Form.Label>Hình ảnh</Form.Label>
                                 <Form.Control
                                   // onChange={handleImageChange}
                                   id="File_image"
+                                  required="required"
                                   type="file"
                                 />
                               </Form.Group>
@@ -276,9 +497,10 @@ const Accordion = () => {
                             <Button onClick={handleClose} variant="secondary">
                               Đóng
                             </Button>
-                            <Button variant="primary" onClick={handleSave}>
+                            <Button variant="primary" onClick={SaveUpdateProduct}>
                               Lưu
                             </Button>
+                            <ToastContainer />
                           </Modal.Footer>
                         </Modal>
                         <AiFillDelete
